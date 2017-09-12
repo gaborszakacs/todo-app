d26fd40fa42f34dd54863de9714a4446fcd3aa45
Gabor Szakacs <gabor@simp.hu>
2017-09-12 12:21:26 +0200

    Add ItemsController

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :items
 end
